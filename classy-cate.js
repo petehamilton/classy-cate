0037de01617769bcaf945e3c5fba82ed8dd18017
Peter Hamilton <peter@inspiredpixel.net>
2013-01-31 23:53:01 +0000

    Added base files for development

---
+++ classy-cate.js
@@ -0,0 +1,5 @@
+/*
+ * @name         CATE 2.0
+ * @author       Pater Hamilton
+ * @description  CATE hurts my eyes. This makes it hurt them less
+ */
---
+++ classy-cate.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name         CATE 2.0
+// @author       Pater Hamilton
+// @description  CATE Hurts eyes. This makes it hurt them less.
+// @match https://cate.doc.ic.ac.uk/*
+// ==/UserScript==
