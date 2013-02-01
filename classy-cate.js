32aaf9c1e99bab45b1cac2b19d2ddef830af003b
Peter Hamilton <peter@inspiredpixel.net>
2013-02-01 02:37:13 +0000

    Updated class selection form

--- classy-cate.js
+++ classy-cate.js
@@ -32,6 +32,7 @@
     ////////////////////////////////////////////////////////////////////////////
     navbar = $('<div class="navbar" />');
     navbar_inner = $('<div class="navbar-inner" />');
+    navbar_container= $('<div class="container" />');
     navbar_links = $('<ul class="nav" />');
 
     //TODO: Work out the active link
@@ -46,16 +47,29 @@
     year_dropdown = $('<li class="dropdown" />');
     year_dropdown_list = $('<ul class="dropdown-menu" role="menu" />');
     year_dropdown.append(year_dropdown_list);
-
-    navbar_branding = $('<a class="brand" href="/">CATE <span id="cate-version">X</span></a>');
-    navbar_inner.append(navbar_branding);
-    navbar_inner.append(navbar_links);
-    navbar_inner.append(year_dropdown_container);
     year_dropdown_container.append(year_dropdown);
 
-    navbar.append(navbar_inner);
+    navbar_collapsed_bars = '<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">' +
+                            '  <span class="icon-bar"></span>' +
+                            '  <span class="icon-bar"></span>' +
+                            '  <span class="icon-bar"></span>' +
+                            '</a>';
+    navbar_branding = $('<a class="brand" href="/">CATE <span id="cate-version">X</span></a>');
+    navbar_collapsed = $('<div class="nav-collapse collapse" />');
 
+    navbar_collapsed.append(navbar_collapsed);
+    navbar_collapsed.append(navbar_branding);
+    navbar_collapsed.append(navbar_links);
+    navbar_collapsed.append(year_dropdown_container);
+
+    navbar_container.append(navbar_collapsed_bars);
+    navbar_container.append(navbar_branding);
+    navbar_container.append(navbar_collapsed);
+
+    navbar_inner.append(navbar_container);
+    navbar.append(navbar_inner);
     container.prepend(navbar);
+
 
 
     ////////////////////////////////////////////////////////////////////////////
@@ -85,6 +99,7 @@
 
     // Remodel the profile table
     main_cate_table = $('table:eq(2)');
+    console.log(main_cate_table);
     title_row = main_cate_table.find('td table td table tr:eq(0)');
     profile_image = title_row.find('img');
     profile_image.removeAttr('height');
@@ -113,18 +128,90 @@
 
 
     ////////////////////////////////////////////////////////////////////////////
-    // Add nicer year selection ////////////////////////////////////////////////
+    // Add nicer exercise timetable selection //////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////
     classes_container = $('<div class="span4"/>');
     classes_container.append('<h1 class="section-header">Exercise Timetable</h1>');
-    classes_container.append(main_cate_table.find('form:first'));
+
+    period_select = $('<select id="period-select" name="period" />');
+    period_select.append('<option value="1">Autumn Term</option>');
+    period_select.append('<option value="2">Christmas</option>');
+    period_select.append('<option value="3">Spring Term</option>');
+    period_select.append('<option value="4">Easter</option>');
+    period_select.append('<option value="5">Summer Term</option>');
+    period_select.append('<option value="6">June-July</option>');
+    period_select.append('<option value="7">August-September</option>');
+    default_period = $('input[name=period]:checked').val();
+    period_select.val(default_period);
+    class_select = $('<select id="class-select" name="class" />');
+    class_select.append('<option value="c1">COMP 1</option>');
+    class_select.append('<option value="c2">COMP 2</option>');
+    class_select.append('<option value="c3">COMP 3</option>');
+    class_select.append('<option value="c4">COMP 4</option>');
+
+    class_select.append('<option value="j1">JMC 1</option>');
+    class_select.append('<option value="j2">JMC 2</option>');
+    class_select.append('<option value="j3">JMC 3</option>');
+    class_select.append('<option value="j4">JMC 4</option>');
+
+    class_select.append('<option value="i2">ISE 2</option>');
+    class_select.append('<option value="i3">ISE 3</option>');
+    class_select.append('<option value="i4">ISE 4</option>');
+
+    class_select.append('<option value="v5">Computing</option>');
+    class_select.append('<option value="s5">Computing Spec.</option>');
+    class_select.append('<option value="a5">Advanced</option>');
+    class_select.append('<option value="r5">Research</option>');
+    class_select.append('<option value="y5">Industrial</option>');
+    class_select.append('<option value="b5">Bioinformatic</option>');
+
+    class_select.append('<option value="occ">Occasional</option>');
+    class_select.append('<option value="r6">PhD</option>');
+    class_select.append('<option value="ext">External</option>');
+
+    default_class = $('input[name=class]:checked').val();
+    class_select.val(default_class);
+
+    hidden_details = $('input[type=hidden]');
+    keyt = hidden_details.val();
+    hidden_details.attr('id', 'keyt');
+
+    submission_button = $('<button class="btn">Go</button>');
+
+    form = $(  '<form class="form-horizontal" action="/timetable.cgi" type="GET" >' +
+               '    <div class="control-group">' +
+               '        <label class="control-label" for="period-select">Period</label>' +
+               '        <div id="period-select-container" class="controls">' +
+               '        </div>' +
+               '    </div>' +
+               '    <div class="control-group">' +
+               '        <label class="control-label" for="class-select">Class</label>' +
+               '        <div id="class-select-container" class="controls">' +
+               '        </div>' +
+               '    </div>' +
+               '    <div class="control-group">' +
+               '        <div id="button-container" class="controls">' +
+               '        </div>' +
+               '    </div>' +
+               '</form>'
+             );
+    form.find('#period-select-container').append(period_select);
+    form.find('#class-select-container').append(class_select);
+    form.find('#button-container').append(submission_button);
+    form.append(hidden_details);
+
+    timetable_url = '/timetable.cgi?period=' + default_period + '&class=' + default_class + '&keyt=' + keyt;
+    $('#nav-exercises').attr('href', timetable_url);
+
+    classes_container.append(form);
     identity_row.append(classes_container);
 
 
     ////////////////////////////////////////////////////////////////////////////
     // More CATE Links /////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////
-    links_tds = main_cate_table.find('tr td ul table:eq(3) tr td:nth-child(3)');
+    console.log(main_cate_table);
+    links_tds = main_cate_table.find('tr td ul table:eq(9) tr td:nth-child(3)');
     grading_schema_href = $(links_tds[1]).find('a').attr('href');
     documentation_href = $(links_tds[2]).find('a').attr('href');
     late_sub_href = $(links_tds[3]).find('a').attr('href');
@@ -155,9 +242,11 @@
     ////////////////////////////////////////////////////////////////////////////
     footer_html =  '<footer class="footer">';
     footer_html += '    <div class="container">';
-    footer_html += '        <p>Designed and Created by <a href="http://www.inspiredpixel.net">Peter Hamilton</a> ';
-    footer_html += '<a href="http://www.twitter.com/peterejhamilton"><i class="icon-twitter" /></a> ';
-    footer_html += '<a href="http://www.github.com/peterejhamilton"><i class="icon-github" /></a> ';
+    footer_html += '        <p>';
+    footer_html += '            Designed and Created by <a href="http://www.inspiredpixel.net">Peter Hamilton</a> ';
+    footer_html += '            <a href="http://www.twitter.com/peterejhamilton"><i class="icon-twitter" /></a> ';
+    footer_html += '            <a href="http://www.github.com/peterejhamilton"><i class="icon-github" /></a> ';
+    footer_html += '        </p>';
     footer_html += '        <p>Help lessen the pain - <a href="#"><i class="icon-github-alt" /></a></p>';
     footer_html += '    </div>';
     footer_html += '</footer>';
