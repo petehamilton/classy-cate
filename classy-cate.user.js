ce58d63a420a36dc2cc972965fac2377976496fd
Peter Hamilton <peter@inspiredpixel.net>
2013-02-01 01:03:10 +0000

    Updated User Script to reference the static github js/css files

--- classy-cate.user.js
+++ classy-cate.user.js
@@ -4,3 +4,31 @@
 // @description  CATE Hurts eyes. This makes it hurt them less.
 // @match https://cate.doc.ic.ac.uk/*
 // ==/UserScript==
+
+function main() {
+    console.log("Giving CATE a makeover");
+
+    // Let's Bootstrap this shizzle. Files from http://www.bootstrapcdn.com/?v=01282013154951
+    $('head').append('<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" type="text/css" />');
+    $('head').append('<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.0/css/font-awesome.css" type="text/css" />');
+    $('head').append('<link rel="stylesheet" href="https://raw.github.com/PeterHamilton/classy-cate/master/classy-cate.css" type="text/css" />');
+
+    $('head').append('<script type="text/javascript" src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script>');
+    $('head').append('<script type="text/javascript" src="https://raw.github.com/PeterHamilton/classy-cate/master/classy-cate.js"></script>');
+}
+
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    // script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+    script.textContent = "(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
