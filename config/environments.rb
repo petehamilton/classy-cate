eef3519bb90f2b202afa2bf4d1d053c806bb1560
Peter Hamilton <peter@inspiredpixel.net>
2013-03-30 01:27:42 +0000

    Modify Cate CSS url depending on env

--- config/environments.rb
+++ config/environments.rb
@@ -1,7 +1,9 @@
 configure :development, :test do
   set :asset_cache_for, 1 # 1 second
+  set :cc_css_url, 'https://localhost:4567/classy-cate.css'
 end
 
 configure :production do
   set :asset_cache_for, 60*60*24 # 1 day
+  set :cc_css_url, 'https://classy-cate.herokuapp.com/classy-cate.css'
 end
