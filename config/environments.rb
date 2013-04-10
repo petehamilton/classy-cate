819078b7ac9a66a77e518c24155093a80c96b30c
Peter Hamilton <pete@gocardless.com>
2013-04-10 02:49:34 +0100

    Merge JS and CSS into single assets

--- config/environments.rb
+++ config/environments.rb
@@ -1,11 +1,9 @@
 configure :development, :test do
   set :asset_cache_for, 1 # 1 second
   set :cc_css_url, 'https://localhost:4567/classy-cate.css'
-  set :tl_css_url, 'https://localhost:4567/timeline.css'
 end
 
 configure :production do
   set :asset_cache_for, 60*60*24 # 1 day
   set :cc_css_url, 'https://classy-cate.herokuapp.com/classy-cate.css'
-  set :tl_css_url, 'https://classy-cate.herokuapp.com/timeline.css'
 end
--- config/version.rb
+++ config/version.rb
@@ -1,2 +1,2 @@
 # Only bump this if you change the userscript
-set :userscript_version, "1.3"
+set :userscript_version, "1.2"
